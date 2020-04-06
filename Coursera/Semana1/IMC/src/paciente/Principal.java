1b66e28fe9439ac1198243bd68a1d592095ab622
KalebeLopes <calbkalebe@gmail.com>
2020-04-06 12:33:07 -0300

    add funcionalidades

--- Coursera/Semana1/IMC/src/paciente/Principal.java
+++ Coursera/Semana1/IMC/src/paciente/Principal.java
@@ -5,10 +5,20 @@ public class Principal {
 	public static void main(String[] args) {
 		
 		Paciente p1 = new Paciente(70, 1.75);
+		Paciente p2 = new Paciente(80, 1.75);
+		Paciente p3 = new Paciente(150, 1.75);
 		
-		p1.calcularIMC();
-		System.out.println(p1.calcularIMC());
+		System.out.println("IMC: " + p1.calcularIMC());
+		System.out.print("Diagnóstico: ");
 		p1.diagnostico();
+		
+		System.out.println("IMC: " + p2.calcularIMC());
+		System.out.print("Diagnóstico: ");
+		p2.diagnostico();
+		
+		System.out.println("IMC: " + p3.calcularIMC());
+		System.out.print("Diagnóstico: ");
+		p3.diagnostico();
 		
 	}
 
